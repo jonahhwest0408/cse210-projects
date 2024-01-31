d6eff8882b435d4f9e7ff53756afe302a8b9aa9d
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-01-31 14:31:26 -0700

    Assignment Topics Program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennet", "Math");
+        Console.WriteLine(a1.GetSummary());
+        
+        MathAssignment a2 = new MathAssignment("Jonah West", "Division", "4", "19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Jonah West", "History", "The Black Plague");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.WritingInformation());
     }
 }
