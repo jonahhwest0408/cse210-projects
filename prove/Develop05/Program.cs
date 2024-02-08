45feebb2c298d56e904b62d89337dc6c43027d0e
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-02-08 08:30:09 -0700

    Extra Credit

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,3 +1,4 @@
+//In order to receieve he extra credit, I implemented a debugging redirect in order to avoid getting an IO error. I also implemented error messages to help guide the user through error.//
 using System;
 
 class Program
